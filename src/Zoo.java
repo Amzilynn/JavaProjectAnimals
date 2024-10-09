43add5a266e23cd4fb54d4e69560a968bfc34727
Amzilynn <152219588+Amzilynn@users.noreply.github.com>
2024-10-09 22:37:52 +0100

    Prosit 2

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,3 +1,24 @@
 public class Zoo {
+    Animal [] animals ;
+    String name;
+    String city;
+    int nbrCages = 25 ;
+
+    public Zoo(String name, String city, int nbrCages) {
+        animals =new Animal[nbrCages];
+        this.name = name;
+        this.city = city;
+        this.nbrCages = nbrCages;
+
+    }
+
+    void displayZoo() {
+        System.out.println("Name: " + name + ", City: " + city + ", N° Cages: " + nbrCages );
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", City: " + city + ", N° Cages: " + nbrCages ;
+    }
 
 }
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -2,17 +2,8 @@ import java.util.Scanner;
 
 public class ZooManagement {
     public static void main(String[] args) {
-        int nbrCages ;
-        String zooName ;
-        Scanner scan = new Scanner(System.in);
 
-        //System.out.println(zooName + " comporte "+ nbrCages+" cages ");
-        System.out.println("Enter the number of Cages: ");
-        nbrCages = scan.nextInt();
-
-        System.out.println("Enter the name of the zoo: ");
-        zooName = scan.next();
-
-        System.out.println(zooName + " comporte "+ nbrCages + " Cages");
+        Animal animal1  = new Animal("Felidae","lion",2,true);
+        Zoo zoo1 = new Zoo("myZoo","tunis",20);
     }
 }
